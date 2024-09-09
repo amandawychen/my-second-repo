5dc671c4f3db7cece588e536b6cc242923d13193
amandawychen <amandawychen@gmail.com>
2024-09-08 18:51:11 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    echo "Welcome"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DevOps-Day8-Second Repo</title>
+</head>
+<body>
+    <h1>DevOps</h1>
+</body>
+</html>
